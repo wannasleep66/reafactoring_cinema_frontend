3204d0b0022658020e89cd54239112ef6bf376eb
Daniil <daniil.karpenk0@yandex.ru>
2025-11-17 19:05:58 +0500

    feat: Добавлено получение API_URL из настроек окружения

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
+
 const api = axios.create({
-  baseURL: "http://91.142.94.183:8080", 
-  withCredentials: true, 
+  baseURL: API_URL,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
