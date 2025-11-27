3b4d58f6f40aa90e7566ddfd3a0045e4b29f9f0f
Daniil <daniil.karpenk0@yandex.ru>
2025-11-27 17:08:42 +0500

    refactor: Убраны неиспользуемые переменные

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -4,37 +4,26 @@ import MovieCard from "./MovieCard";
 import MovieDetailsPage from "./MovieDetailsPage";
 import { getFilms, type Film } from "./api/movie";
 
-let globalLoadingState = false;
-
 export default function HomePage() {
   const [films, setFilms] = useState<Film[]>([]);
   const [selectedFilm, setSelectedFilm] = useState<Film | null>(null);
-  const [viewCount, setViewCount] = useState(0);
 
   useEffect(() => {
-    globalLoadingState = true;
-
     getFilms()
       .then((filmsData) => {
         setFilms(filmsData.data);
-        globalLoadingState = false;
-
-        setViewCount((prev) => prev + 1);
       })
       .catch((error) => {
         console.error("Ошибка загрузки фильмов:", error);
-        globalLoadingState = false;
       });
   }, []);
 
   const handleFilmSelect = (film: Film) => {
     setSelectedFilm(film);
-    setViewCount((prev) => prev + 1);
   };
 
   const handleBackFromDetails = () => {
     setSelectedFilm(null);
-    setViewCount((prev) => prev + 1);
   };
 
   if (selectedFilm) {
