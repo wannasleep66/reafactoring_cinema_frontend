e562f392207288f6b83a6e605c3e9363946acad2
Daniil <daniil.karpenk0@yandex.ru>
2025-12-11 22:53:46 +0500

    'Условная сложность' исправлен с помощью 'Извлечение метода'. Формирование стилей для кнопки вынесено в утилиту, что позволит задать 1 метод для формирования динамических классов и снизит количество повторяющегося сложного кода

--- src/utils/clsx.ts
+++ src/utils/clsx.ts
@@ -1,19 +1,22 @@
 export function clsx(
   args: Array<string | { [key: string]: boolean }> | string
-) {
+): string {
   if (typeof args === "string") {
     return args;
   }
 
-  return args.reduce((className, arg) => {
-    if (typeof arg === "string") {
-      return className ? `${className} ${arg}` : arg;
-    } else {
-      const keys = Object.keys(arg).filter((key) => arg[key]);
-      const filteredClassNames = keys.join(" ");
-      return className
-        ? `${className} ${filteredClassNames}`
-        : filteredClassNames;
-    }
-  }, "");
+  return args.reduce(
+    (className: string, arg: string | { [key: string]: boolean }) => {
+      if (typeof arg === "string") {
+        return className ? `${className} ${arg}` : arg;
+      } else {
+        const keys = Object.keys(arg).filter((key) => arg[key]);
+        const filteredClassNames = keys.join(" ");
+        return className
+          ? `${className} ${filteredClassNames}`
+          : filteredClassNames;
+      }
+    },
+    ""
+  );
 }
